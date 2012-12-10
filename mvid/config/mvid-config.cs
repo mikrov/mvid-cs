bc609f70bbb5e908ee66aab1d0df7452a73d1442
Brian Simonsen <brs@ubuntu.(none)>
2012-12-10 16:33:02 +0100

    * mvid-config - added a new read function. * mvid-handler - changed to use the new read function in mvid-config

--- mvid/config/mvid-config.cs
+++ mvid/config/mvid-config.cs
@@ -3,45 +3,73 @@ using System.Collections;
 
 class mvidconfig
 {
-  static public Hashtable mvid_config = new Hashtable();
-  static public Hashtable challenge = new Hashtable();
-  static public Hashtable mvid = new Hashtable();
-  static public Hashtable storage = new Hashtable();
-  static public Hashtable memcache = new Hashtable();
+	static private Hashtable mvid_config = new Hashtable();
+	static private Hashtable mvid = new Hashtable();
+	static private Hashtable challenge = new Hashtable();
+	static private Hashtable storage = new Hashtable();
+	static private Hashtable memcache = new Hashtable();
 
-  static mvidconfig(){
-    mvid_config.Add("mvid", mvid);
-    mvid.Add("challenge", challenge);
-    mvid.Add("storage", storage);
+	static mvidconfig(){
+		mvid_config.Add("mvid", mvid);
+		mvid.Add("challenge", challenge);
+		mvid.Add("storage", storage);
 
-    /* Type in the shared key you recieved from MV-ID after you registered your application domain. If you havn't
-      registered your application domain yet there is a form you can fill in at:
-      https://signon.mv-nordic.com/wiki/app-domain-reg */
-    challenge.Add("shared_key" , "<shared-key>");
+		/* Type in the shared key you recieved from MV-ID after you registered your application domain. If you havn't
+		registered your application domain yet there is a form you can fill in at:
+		https://signon.mv-nordic.com/wiki/app-domain-reg */
+		challenge.Add("shared_key" , "<shared_key>");
 
-    /* The domain you wish to register the user sessions with. The base domain of this value must match the
-      application domain you have registered with MV-ID */
-    challenge.Add("domain" , "<application-domain>");
+		/* The domain you wish to register the user sessions with. The base domain of this value must match the
+		application domain you have registered with MV-ID */
+		challenge.Add("domain", "<application-domain>");
 
-    challenge.Add("redirect_on_success" , "start.aspx");
-    challenge.Add("redirect_on_failure" , "error.aspx");
+		challenge.Add("redirect_on_success" , "start.aspx");
+		challenge.Add("redirect_on_failure" , "error.aspx");
 
-    /* The identifier name to store the mv_session_id with.
+		/* The identifier name to store the mv_session_id with.
 
-      Default: "mv_session_id" */
-    storage.Add("name" , "mv_session_id");
+		Default: "mv_session_id" */
+		storage.Add("name" , "mv_session_id");
 
-    /* Choose how to store the mv_session_id:
-      1. "cookie"   - If you plan to develope an application that communicates with MV-ID's webservices via javascript
-                      you must store the mv_session_id as a session cookie on the client's browser.
+		/* Choose how to store the mv_session_id:
+		1. "cookie"   - If you plan to develope an application that communicates with MV-ID's webservices via javascript
+		you must store the mv_session_id as a session cookie on the client's browser.
 
-      2. "csharp"   - On the other hand, if you are going to do all communications to MV-ID's webservices from the
-                      serverside, you might as well store the mv_session_ids as a C# session variable.
+		2. "csharp"   - On the other hand, if you are going to do all communications to MV-ID's webservices from the
+		serverside, you might as well store the mv_session_ids as a C# session variable.
 
-      3.            - If you want to store your mv_session_id a completely different way you can extend the with a new
-                      session storage class by adding it in the directory sessionstorage.
+		3.- If you want to store your mv_session_id a completely different way you can extend the with a new
+		session storage class by adding it in the directory sessionstorage.
 
-      Default: "cookie" */
-    storage.Add("type" , "cookie");
-  }
+		Default: "cookie" */
+		storage.Add("type" , "cookie");
+	}
+
+	static public string read_config(Array path, string default_val)
+	{
+		Hashtable current_crumb = mvid_config;
+		foreach (string v in path)
+		{
+			if (current_crumb[v] != null)
+			{
+				if (current_crumb[v].GetType() == typeof(Hashtable))
+				{
+					current_crumb = (Hashtable)current_crumb[v];
+				}
+				else
+				{
+					return current_crumb[v].ToString();
+				}
+			}
+			else
+			{
+				if (default_val != null)
+				{
+					return default_val;
+				}
+				return null;
+			}
+		}
+		return current_crumb.Values.ToString();
+	}
 }
--- mvid/mvid-handler.cs
+++ mvid/mvid-handler.cs
@@ -8,88 +8,88 @@ using System.Collections;
 
 public class mvid_handler
 {
-  public string mv_session_id;
+	public string mv_session_id;
 
-  public mvid_handler()
-  {
-    mvid_challenge mvc = new mvid_challenge();
-    mv_session_id = "";
-    string error_message = "";
-    string mvid_app_domain = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["challenge"])["domain"].ToString();
-    string mvid_shared_key = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["challenge"])["shared_key"].ToString();
-    string mvid_redirect_on_success = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["challenge"])["redirect_on_success"].ToString();
-    string mvid_redirect_on_failure = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["challenge"])["redirect_on_failure"].ToString();
-    string mvid_storage_type = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["storage"])["type"].ToString();
-    string mvid_storage_name = ((Hashtable)((Hashtable)mvidconfig.mvid_config["mvid"])["storage"])["name"].ToString();
+	public mvid_handler()
+	{
+		mvid_challenge mvc = new mvid_challenge();
+		mv_session_id = "";
+		string error_message = "";
+		string mvid_app_domain = mvidconfig.read_config(new string[] { "mvid", "challenge", "domain" }, null);
+		string mvid_shared_key = mvidconfig.read_config(new string[] { "mvid", "challenge","shared_key"}, null);
+		string mvid_redirect_on_success = mvidconfig.read_config(new string[] { "mvid", "challenge","redirect_on_success"}, null);
+		string mvid_redirect_on_failure = mvidconfig.read_config(new string[] { "mvid", "challenge","redirect_on_failure"}, null);
+		string mvid_storage_type = mvidconfig.read_config(new string[] { "mvid", "storage","type"}, null);
+		string mvid_storage_name = mvidconfig.read_config(new string[] { "mvid", "storage","name"}, null);
 
-    // Not sure what storage_class is!
-    MVID_SessionStorage storage_class = null;
-    NameValueCollection nvc = HttpContext.Current.Request.Form;
+		// Not sure what storage_class is!
+		MVID_SessionStorage storage_class = null;
+		NameValueCollection nvc = HttpContext.Current.Request.Form;
 
-    if(mvid_storage_type != null)
-    {
-      try
-      {
-        Type storage_class_type = Type.GetType(mvid_storage_type + "SessionStorage");
-        storage_class = (MVID_SessionStorage) Activator.CreateInstance(storage_class_type);
-      }
-      catch
-      {
-        error_message = "The storage type: " + mvid_storage_type + " is not available.";
-      }
-    }
+		if(mvid_storage_type != null)
+		{
+			try
+			{
+				Type storage_class_type = Type.GetType(mvid_storage_type + "SessionStorage");
+				storage_class = (MVID_SessionStorage) Activator.CreateInstance(storage_class_type);
+			}
+			catch
+			{
+				error_message = "The storage type: " + mvid_storage_type + " is not available.";
+			}
+		}
 
-    if (!string.IsNullOrEmpty(nvc["mv_session_hash"]))
-    {
-      // A session hash was recieved from MV-ID either after a doLogin() or a doSSO()
-      Boolean success = false;
+		if (!string.IsNullOrEmpty(nvc["mv_session_hash"]))
+		{
+			// A session hash was recieved from MV-ID either after a doLogin() or a doSSO()
+			Boolean success = false;
 
-      if (storage_class != null) 
-      {
-        // Register the session hash for application usage:
-        success = mvc.registerSessionUsage(nvc["mv_session_hash"], mvid_app_domain, mvid_shared_key, ref mv_session_id, ref error_message);
-      }
+			if (storage_class != null) 
+			{
+				// Register the session hash for application usage:
+				success = mvc.registerSessionUsage(nvc["mv_session_hash"], mvid_app_domain, mvid_shared_key, ref mv_session_id, ref error_message);
+			}
 
-      if (success)
-      {
-        storage_class.save(mvid_storage_name, mv_session_id);
+			if (success)
+			{
+				storage_class.save(mvid_storage_name, mv_session_id);
 
-        if (mvid_redirect_on_success != null) 
-        {
-          HttpContext.Current.Response.Redirect(mvid_redirect_on_success);
-        }
-      }
-      else
-      {
-        if (mvid_redirect_on_failure != null)
-        {
-          HttpContext.Current.Response.Redirect(mvid_redirect_on_failure + "?error_message=" + HttpUtility.UrlEncode(error_message));
-        }
-        else
-        {
-          HttpContext.Current.Response.Write("Error: " + error_message);
-          Environment.Exit(-1);
-        }
-      }
-    }
-    else
-    {
-      if (storage_class == null)
-      {
-        if (mvid_redirect_on_failure != null)
-        {
-          HttpContext.Current.Response.Redirect(mvid_redirect_on_failure + "?error_message=" + HttpUtility.UrlEncode(error_message));
-        }
-        else
-        {
-          HttpContext.Current.Response.Write("Error: " + error_message);
-          Environment.Exit(-1);
-        }
-      }
-      else
-      {
-        mv_session_id = storage_class.load(mvid_storage_name);
-      }
-    }
-  }
+				if (mvid_redirect_on_success != null) 
+				{
+					HttpContext.Current.Response.Redirect(mvid_redirect_on_success);
+				}
+			}
+			else
+			{
+				if (mvid_redirect_on_failure != null)
+				{
+				HttpContext.Current.Response.Redirect(mvid_redirect_on_failure + "?error_message=" + HttpUtility.UrlEncode(error_message));
+				}
+				else
+				{
+				HttpContext.Current.Response.Write("Error: " + error_message);
+				Environment.Exit(-1);
+				}
+			}
+		}
+		else
+		{
+			if (storage_class == null)
+			{
+				if (mvid_redirect_on_failure != null)
+				{
+					HttpContext.Current.Response.Redirect(mvid_redirect_on_failure + "?error_message=" + HttpUtility.UrlEncode(error_message));
+				}
+				else
+				{
+				HttpContext.Current.Response.Write("Error: " + error_message);
+				Environment.Exit(-1);
+				}
+			}
+			else
+			{
+				mv_session_id = storage_class.load(mvid_storage_name);
+			}
+		}
+	}
 }
